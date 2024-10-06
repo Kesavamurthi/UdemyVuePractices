332257c2697eaa1bec682d205d12849fcdcb6c55
Kesava M <kesavamkv@gmail.com>
2024-10-06 23:14:56 +0530

    event modifiers were practiced]

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
     };
   },
+  methods:{
+    add(){
+        this.counter = this.counter + 1;
+    },
+    removethis(){
+        this.counter = this.counter - 1;
+    },
+    setName(event){
+        this.name = event.target.value;
+    },
+    submitForm(){
+        alert('submitted');
+    },
+    setConfirmedName(){
+        this.confirmedName = this.name;
+    }
+  }
 });
 
 app.mount('#events');
